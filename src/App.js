e2b489747370f81b9abac9ec1e01f93134e3f74f
hardi <hardi@outlook.com>
2020-09-18 15:23:32 +0800

    add in-construction page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import HeaderComponent from './components/header/header';
 import './App.css';
+import HeaderComponent from './components/header/header';
+import InConstruction from './pages/in-construction/inConstruction';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent></HeaderComponent>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div className="page-content-wrap">
+        <InConstruction></InConstruction>
+      </div>
     </div>
   );
 }
---
+++ src/pages/in-construction/inConstruction.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./inConstruction.scss";
+
+export default class InConstruction extends Component {
+  render() {
+    return (
+      <div className="in-construction-page">
+        <div className="card">
+            <div className="content">THIS SITE IS UNDER CONSTRUCTION</div>
+        </div>
+      </div>
+    )
+  }
+}
